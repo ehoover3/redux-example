2e81ac748119bae832a668a468bb8e990f345663
Eric Hoover <ehoover3@yahoo.com>
2022-04-21 20:46:00 -0700

    removed unused variables

--- src/components/Panda.js
+++ src/components/Panda.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { add, subtract, addByAmount } from "../store/animalStore";
 
 export default function Snakes() {
   // STORE
-  const count = useSelector((state) => state.counter.value); // get initial value from store
   const dispatch = useDispatch(); // send info (a.k.a. "state") to the store
 
   // USESTATE
--- src/components/SeaTurtle.js
+++ src/components/SeaTurtle.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { add, subtract } from "../store/animalStore";
 
 export default function SeaTurtle() {
   // STORE
-  const count = useSelector((state) => state.counter.value); // get initial value from store
   const dispatch = useDispatch(); // send info (a.k.a. "state") to the store
 
   return (
